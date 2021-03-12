589c480075d2b19a12539cf092af8d29739d3682
Guillaume Téchené <guillaume.techene@outlook.com>
2021-03-12 11:31:57 +0100

    First commit

---
+++ FruitShop.Tests/FruitShopShould.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NFluent;
+using NUnit.Framework;
+
+namespace FruitShop.Tests
+{
+    public class FruitShopShould
+    {
+        [Test]
+        public void Display_Pommes_Price()
+        {
+            var cashRegister = new CashRegister();
+            var price = cashRegister.Add("Pommes");
+
+            Check.That(price).IsEqualTo(100);
+        }
+
+        [Test]
+        public void Display_Cerises_Price()
+        {
+            var cashRegister = new CashRegister();
+            var price = cashRegister.Add("Cerises");
+
+            Check.That(price).IsEqualTo(75);
+        }
+    }
+
+    public class CashRegister
+    {
+        public int Add(string product)
+        {
+            if (product == "Pommes")
+            {
+                return 100;
+            }
+
+            return 75;
+        }
+    }
+}
